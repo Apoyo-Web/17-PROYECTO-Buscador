9afd9e45d8a4a982b535bb609af3641dee73b23d
apoyo web <mpienso@gmail.com>
2020-12-14 20:21:44 +0100

    entrar

--- js/app.js
+++ js/app.js
@@ -47,23 +47,28 @@ year.addEventListener('change', e => {
 })
 
 minimo.addEventListener('change', e => {
-    datosBusqueda.minimo = e.target.value;
+    datosBusqueda.minimo = parseInt(e.target.value);
+    filtrarAuto();
 })
 
 maximo.addEventListener('change', e => {
-    datosBusqueda.maximo = e.target.value;
+    datosBusqueda.maximo = parseInt(e.target.value);
+    filtrarAuto();
 })
 
 puertas.addEventListener('change', e => {
-    datosBusqueda.puertas = e.target.value;
+    datosBusqueda.puertas = parseInt(e.target.value);
+    filtrarAuto();
 })
 
 transmision.addEventListener('change', e => {
     datosBusqueda.transmision = e.target.value;
+    filtrarAuto();
 })
 
 color.addEventListener('change', e => {
     datosBusqueda.color = e.target.value;
+    filtrarAuto();
 })
 
 
@@ -105,26 +110,82 @@ function llenarSelect() {
 
 
 function filtrarAuto() {
-    const resultado = autos.filter(filtrarMarca).filter(filtrarYear);
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor);
     //console.log(resultado);
 
-    mostrarAutos(resultado);    
+    if (resultado.length) {
+        mostrarAutos(resultado);
+    } else {
+        noResultado();
+    }    
+}
+
+function noResultado() {
+
+    limpiarHTML();
+
+    const noResultado = document.createElement('div');
+    noResultado.classList.add('alerta', 'error');
+    noResultado.textContent = 'No hay resultados';
+    resultado.appendChild(noResultado);
+
 }
 function filtrarMarca(auto) {
     const { marca } = datosBusqueda;
     if ( marca ) {
         return auto.marca === marca;
-    } else {
+    } 
         return auto;
-    }
+    
 }
 
 function filtrarYear(auto) {
     const { year } = datosBusqueda;
     if (year) {
         return auto.year === year;
-    } else {
-        return auto;
-    }
+    } 
+    return auto;
     
+}
+
+function filtrarMinimo(auto) {
+    const { minimo } = datosBusqueda;
+    if (minimo) {
+        return auto.precio >= minimo;
+    } 
+    return auto;
+}
+
+function filtrarMaximo(auto) {
+    const { maximo } = datosBusqueda;
+    if (maximo) {
+        return auto.precio <= maximo;
+    }
+
+    return auto;
+}
+
+function filtrarPuertas(auto) {
+    const { puertas } = datosBusqueda;
+    if (puertas) {
+        return auto.puertas === puertas;
+    }
+    return auto;
+}
+
+function filtrarTransmision(auto) {
+    const { transmision } = datosBusqueda;
+    if (transmision) {
+        return auto.transmision == transmision;
+    }
+    return auto;
+}
+
+function filtrarColor(auto){
+    const { color } = datosBusqueda;
+    if (color) {
+        return auto.color == color;
+
+    }
+    return auto;
 }
